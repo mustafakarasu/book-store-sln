2c73316574893460859164251afc2af4edc3cc30
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-01-09 22:40:11 +0300

    UpdateBook ve UpdateBookValidator için unit test kodları yazıldı.

---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/UpdateBook/UpdateBookCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using BookStoreWebApi.Application.BookOperations.Commands.CreateBook;
+using BookStoreWebApi.DBOperations;
+using BookStoreWebApi.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreWebApi.Application.BookOperations.Commands.UpdateBook;
+using TestSetup;
+using Xunit;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.UpdateBook
+{
+    public class UpdateBookCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public UpdateBookCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            //arrange
+            var bookIds = _context.Books.Select(x => x.Id).Take(5).ToList();
+
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.Model = new UpdateBookModel() { Title = "Lord of" };
+
+            //act & assert
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap Bulunamadı");
+        }
+    }
+}
---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/UpdateBook/UpdateBookCommandValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using BookStoreWebApi.Application.BookOperations.Commands.CreateBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreWebApi.Application.BookOperations.Commands.UpdateBook;
+using FluentAssertions;
+using TestSetup;
+using Xunit;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.UpdateBook
+{
+    
+    public class UpdateBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        [Theory]
+        [InlineData("Lord Of The", 0, 0)]
+        [InlineData("", 0, 0)]
+        [InlineData("", 0, 1)]
+        [InlineData("", 100, 1)]
+        [InlineData("Lor", 100, 1)]
+        [InlineData("Lord", 0, 1)]
+        [InlineData("Lord", 100, 0)]
+        public void WhenInvalidInputsAreGiven_Validator_ShouldBeReturnErrors(string title, int pageCount, int genreId)
+        {
+            //arrange
+            UpdateBookCommand command = new UpdateBookCommand(null);
+
+            command.Model = new UpdateBookModel() { Title  = title, PageCount = pageCount, PublishDate = DateTime.Now.AddDays(-10), GenreId = genreId };
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var errors = validator.Validate(command);
+
+            //act & assert
+            errors.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenDateTimeEqualNowIsGiven_Validator_ShouldBeReturnError()
+        {
+            //arrange
+            UpdateBookCommand command = new UpdateBookCommand(null);
+
+            command.Model = new UpdateBookModel() { Title = "Lord Of The Rings", PageCount = 100, PublishDate = DateTime.Now, GenreId = 1 };
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var errors = validator.Validate(command);
+
+            //act & assert
+            errors.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
