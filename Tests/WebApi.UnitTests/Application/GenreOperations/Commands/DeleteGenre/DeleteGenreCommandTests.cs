a8978f9703c5e811b48a96cbf75bc2b7eb7c16b9
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-01-12 11:33:37 +0300

    DeleteGenreCommand ve Validator'ı için unit test kodları yazıldı.

---
+++ Tests/WebApi.UnitTests/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommandTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoMapper;
+using BookStoreWebApi.Application.GenreOperations.Commands.DeleteGenre;
+using BookStoreWebApi.DBOperations;
+using FluentAssertions;
+using TestSetup;
+using Xunit;
+
+namespace WebApi.UnitTests.Application.GenreOperations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DeleteGenreCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(150)]
+        public void WhenThereIsNoGenre_InvalidOperationException_ShouldBeReturn(int genreId)
+        {
+            //arrange
+            DeleteGenreCommand deleteCommand = new DeleteGenreCommand(_context);
+            deleteCommand.GenreId = genreId;
+
+            //act & assert
+            FluentActions.Invoking(() => deleteCommand.Handle()).Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap Türü Bulunamadı"); ;
+        }
+    }
+}
---
+++ Tests/WebApi.UnitTests/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommandValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using BookStoreWebApi.Application.GenreOperations.Commands.DeleteGenre;
+using FluentAssertions;
+using TestSetup;
+using Xunit;
+
+namespace WebApi.UnitTests.Application.GenreOperations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void WhenInvalidGenreIdInputsAreGiven_Validator_ShouldBeReturnErrors(int genreId)
+        {
+            //arrange
+            DeleteGenreCommand command = new DeleteGenreCommand(null);
+
+            command.GenreId = genreId;
+            DeleteGenreCommandValidator validator = new DeleteGenreCommandValidator();
+            var errors = validator.Validate(command);
+
+            //act & assert
+            errors.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
